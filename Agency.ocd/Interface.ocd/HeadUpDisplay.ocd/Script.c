4fa1ad740bbc470c83ee04c75f66a2cc39cf1ac4
Mark <markh4b@hotmail.com>
2019-07-04 20:56:16 +0200

    Added basic ingame HUD

---
+++ Agency.ocd/Interface.ocd/HeadUpDisplay.ocd/Script.c
@@ -0,0 +1,5 @@
+// Include the different subsystems of the HUD. They all handle their part
+// themselves via overloading of callbacks.
+#include GUI_Controller_InventoryBar
+// Include the basic functionality of the HUD
+#include Library_HUDController

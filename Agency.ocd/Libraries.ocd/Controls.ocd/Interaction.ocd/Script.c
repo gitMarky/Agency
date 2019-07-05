5be16c90f12847403cbe66d4e43f13782c77520b
Mark <markh4b@hotmail.com>
2019-07-05 21:22:57 +0200

    Simplified interaction logic, code style

--- Agency.ocd/Libraries.ocd/Controls.ocd/Interaction.ocd/Script.c
+++ Agency.ocd/Libraries.ocd/Controls.ocd/Interaction.ocd/Script.c
@@ -13,23 +13,27 @@
 */
 
 
-public func Construction()
+func Construction()
 {
 	this.control.is_interacting = false;
 	return _inherited(...);
 }
 
-public func OnShiftCursor(object new_cursor)
+func OnShiftCursor(object new_cursor)
 {
 	if (this.control.is_interacting)
+	{
 		AbortInteract();
+	}
 	return _inherited(new_cursor, ...);
 }
 
-public func ObjectControl(int plr, int ctrl, int x, int y, int strength, bool repeat, int status)
+func ObjectControl(int plr, int ctrl, int x, int y, int strength, bool repeat, int status)
 {
 	if (!this)
+	{
 		return inherited(plr, ctrl, x, y, strength, repeat, status, ...);
+	}
 
 	// Begin interaction.
 	if (ctrl == CON_Interact && status == CONS_Down)
@@ -58,15 +62,26 @@ public func ObjectControl(int plr, int ctrl, int x, int y, int strength, bool re
 
 		// Switch left/right through objects.
 		var dir = nil;
-		if (ctrl == CON_InteractNext_Left) dir = -1;
-		else if (ctrl == CON_InteractNext_Right) dir = 1;
-		else if (ctrl == CON_InteractNext_CycleObject) dir = 0;
+		if (ctrl == CON_InteractNext_Left)
+		{
+			dir = -1;
+		}
+		else if (ctrl == CON_InteractNext_Right)
+		{
+			dir = 1;
+		}
+		else if (ctrl == CON_InteractNext_CycleObject)
+		{
+			dir = 0;
+		}
 
 		if (dir != nil)
 		{
 			var item = FindNextInteraction(this.control.interaction_hud_controller->GetCurrentInteraction(), dir);
 			if (item)
+			{
 				SetNextInteraction(item);
+			}
 			return true;
 		}
 	}
@@ -76,7 +91,10 @@ public func ObjectControl(int plr, int ctrl, int x, int y, int strength, bool re
 
 private func FxIntHighlightInteractionStart(object target, proplist fx, temp, proplist interaction, int nr_interactions)
 {
-	if (temp) return;
+	if (temp)
+	{
+		return;
+	}
 	fx.obj = interaction.interaction_object;
 	fx.interaction = interaction;
 	fx.interaction_help = target.control.interaction_hud_controller->GetInteractionHelp(interaction, target);
@@ -104,10 +122,14 @@ private func FxIntHighlightInteractionStart(object target, proplist fx, temp, pr
 	}
 	var multiple_interactions_hint = "";
 	if (fx.interaction.has_multiple_interactions)
+	{
 		multiple_interactions_hint = Format("|<c 999999>[%s] $More$..</c>", GetPlayerControlAssignment(GetOwner(), CON_Up, true, false));
+	}
 	var cycle_interactions_hint = "";
 	if (nr_interactions > 1)
+	{
 		cycle_interactions_hint = Format("|<c 999999>[%s/%s] $Cycle$..</c>", GetPlayerControlAssignment(GetOwner(), CON_Left, true, false), GetPlayerControlAssignment(GetOwner(), CON_Right, true, false));
+	}
 	fx.dummy->Message("@<c eeffee>%s</c>%s%s|", fx.interaction_help.help_text, multiple_interactions_hint, cycle_interactions_hint);
 
 	// Center dummy!
@@ -121,13 +143,18 @@ private func FxIntHighlightInteractionStart(object target, proplist fx, temp, pr
 	fx.dummy->SetGraphics(nil, nil, 1, GFXOV_MODE_Object, nil, GFX_BLIT_Additive, fx.obj);
 
 	var custom_selector = nil;
-	if (fx.obj) custom_selector = fx.obj->~DrawCustomInteractionSelector(fx.dummy, target, fx.interaction.interaction_index, fx.interaction.extra_data);
+	if (fx.obj)
+	{
+		custom_selector = fx.obj->~DrawCustomInteractionSelector(fx.dummy, target, fx.interaction.interaction_index, fx.interaction.extra_data);
+	}
 
 	if (!custom_selector)
 	{
 		fx.scheduled_selection_particle = (FrameCounter() - this.control.interaction_start_time) < 10;
 		if (!fx.scheduled_selection_particle)
+		{
 			EffectCall(nil, fx, "CreateSelectorParticle");
+		}
 	}
 	else
 	{
@@ -136,7 +163,7 @@ private func FxIntHighlightInteractionStart(object target, proplist fx, temp, pr
 	}
 }
 
-private func FxIntHighlightInteractionCreateSelectorParticle(object target, effect fx)
+func FxIntHighlightInteractionCreateSelectorParticle(object target, effect fx)
 {
 	// Failsafe.
 	if (!fx.dummy) return;
@@ -153,7 +180,7 @@ private func FxIntHighlightInteractionCreateSelectorParticle(object target, effe
 	fx.dummy->CreateParticle("Selector", 0, 0, 0, 0, 0, Particles_Colored(selector, GetPlayerColor(GetOwner())), 1);
 }
 
-private func FxIntHighlightInteractionTimer(object target, proplist fx, int time)
+func FxIntHighlightInteractionTimer(object target, proplist fx, int time)
 {
 	if (!fx.dummy) return -1;
 	if (!fx.obj) return -1;
@@ -165,14 +192,14 @@ private func FxIntHighlightInteractionTimer(object target, proplist fx, int time
 	}
 }
 
-private func FxIntHighlightInteractionStop(object target, proplist fx, int reason, temp)
+func FxIntHighlightInteractionStop(object target, proplist fx, int reason, temp)
 {
 	if (temp) return;
 	if (fx.dummy) fx.dummy->RemoveObject();
 	if (!this) return;
 }
 
-private func FxIntHighlightInteractionOnExecute(object target, proplist fx)
+func FxIntHighlightInteractionOnExecute(object target, proplist fx)
 {
 	if (!fx.obj || !fx.dummy) return;
 	var message = fx.dummy->CreateObject(FloatingMessage, 0, 0, GetOwner());
@@ -182,7 +209,7 @@ private func FxIntHighlightInteractionOnExecute(object target, proplist fx)
 	message->FadeOut(1, 20);
 }
 
-private func SetNextInteraction(proplist to)
+func SetNextInteraction(proplist to)
 {
 	// Clear all old markers.
 	var e = nil;
@@ -195,7 +222,7 @@ private func SetNextInteraction(proplist to)
 		AddEffect("IntHighlightInteraction", this, 1, 2, this, nil, to, interaction_cnt);
 }
 
-private func FindNextInteraction(proplist start_from, int x_dir)
+func FindNextInteraction(proplist start_from, int x_dir)
 {
 	var starting_object = this;
 	if (start_from && start_from.interaction_object)
@@ -278,7 +305,7 @@ private func FindNextInteraction(proplist start_from, int x_dir)
 	return next;
 }
 
-private func BeginInteract()
+func BeginInteract()
 {
 	this.control.interaction_hud_controller = this->GetHUDController();
 	this.control.is_interacting = true;
@@ -294,13 +321,13 @@ private func BeginInteract()
 }
 
 // Stops interaction selection without executing the current selection.
-private func AbortInteract()
+func AbortInteract()
 {
 	this.control.interaction_hud_controller->SetCurrentInteraction(nil);
 	EndInteract();
 }
 
-private func EndInteract()
+func EndInteract()
 {
 	this.control.is_interacting = false;
 
@@ -323,10 +350,10 @@ private func EndInteract()
 	this.control.interaction_hud_controller->EnableInteractionUpdating(true);
 }
 
-/*
+/**
 	Wraps "PushBack", but also sets a flag when two interactions of the same object exist.
 */
-private func PushBackInteraction(array to, proplist interaction)
+func PushBackInteraction(array to, proplist interaction)
 {
 	PushBack(to, interaction);
 	var count = 0;
@@ -336,13 +363,15 @@ private func PushBackInteraction(array to, proplist interaction)
 		{
 			count += 1;
 			if (count > 1 || other != interaction)
+			{
 				other.has_multiple_interactions = true;
+			}
 		}
 	}
 }
 
-/*
-	returns an array containing proplists with informations about the interactable actions.
+/**
+	Returns an array containing proplists with information about the interactable actions.
 	The proplist properties are:
 		interaction_object
 		priority: used for sorting the objects in the action bar. Note that the returned objects are not yet sorted
@@ -353,120 +382,43 @@ private func PushBackInteraction(array to, proplist interaction)
 func GetInteractableObjects(array sort)
 {
 	var possible_interactions = [];
-	// find vehicles & structures & script interactables
-	// Get custom interactions from the clonk
-	// extra interactions are an array of proplists. proplists have to contain at least a function pointer "f", a description "desc" and an "icon" definition/object. Optional "front"-boolean for sorting in before/after other interactions.
-	var extra_interactions = this->~GetExtraInteractions() ?? []; // if not present, just use []. Less error prone than having multiple if (!foo).
 
-	// all except structures only if outside
-	var can_only_use_container = !!Contained();
-
-	// add extra-interactions
-	if (!can_only_use_container)
-	for (var interaction in extra_interactions)
-	{
-		PushBackInteraction(possible_interactions,
-			{
-				interaction_object = interaction.Object,
-				priority = interaction.Priority,
-				interaction_index = nil,
-				extra_data = interaction,
-				actiontype = ACTIONTYPE_EXTRA
-			});
-	}
+	// All except structures only if outside
+	var can_use_surrounding = !Contained();
 
 	// Make sure that the Clonk's action target is always shown.
 	// You can push a lorry out of your bounding box and would, otherwise, then be unable to release it.
-	var main_criterion = Find_AtRect(-5, -10, 10, 20);
+	var main_criterion = Find_Distance(12);
 	var action_target = GetActionTarget();
 	if (action_target)
 	{
 		main_criterion = Find_Or(main_criterion, Find_InArray([action_target]));
 	}
 
-	// add interactables (script interface)
+	// Add interactables (script interface)
 	var interactables = FindObjects(
 		main_criterion,
-		Find_Or(Find_OCF(OCF_Grab), Find_Func("IsInteractable", this), Find_OCF(OCF_Entrance)),
+		Find_Func("IsInteractable"),
 		Find_NoContainer(), Find_Layer(GetObjectLayer()),
 		sort);
 	for (var interactable in interactables)
 	{
 		var interaction_count = interactable->~GetInteractionCount() ?? 1;
 		var uses_container = interactable == Contained();
-		var can_be_grabbed = interactable->GetOCF() & OCF_Grab;
-		var has_script_interaction = interactable->~IsInteractable(this);
 
-		// handle the script interactions first
-		// one object could have a scripted interaction AND be a vehicle
-		if (has_script_interaction && (!can_only_use_container || uses_container))
-		for (var index = 0; index < interaction_count; index++)
+		if (can_use_surrounding || uses_container)
 		{
-			PushBackInteraction(possible_interactions,
-				{
-					interaction_object = interactable,
-					priority = 9,
-					interaction_index = index,
-					extra_data = nil,
-					actiontype = ACTIONTYPE_SCRIPT
-				});
-		}
-
-		// check whether further interactions are possible
-		// can be grabbed? (vehicles/chests..)
-		if (can_be_grabbed && !can_only_use_container)
-		{
-			var priority = 19;
-			// not if swimming because the grab command cannot really fix that (unlike e.g. scale/hangle)
-			if (GetProcedure() == "SWIM")
-				if (!this->~CanGrabUnderwater(interactable)) // unless it's a special clonk that can grab underwater. It needs to define a callback then.
-					continue;
-			// high priority if already grabbed
-			if (GetActionTarget() == interactable) priority = 0;
-
-			PushBackInteraction(possible_interactions,
-				{
-					interaction_object = interactable,
-					priority = priority,
-					interaction_index = nil,
-					extra_data = nil,
-					actiontype = ACTIONTYPE_VEHICLE
-				});
-		}
-
-		// Can be entered or exited?
-		var has_entrance = interactable->GetOCF() & OCF_Entrance;
-		var can_be_exited = has_entrance && uses_container;
-		// Check if object shape overlaps with entrance area.
-		var can_be_entered = false;
-		if (has_entrance)
-		{
-			var entrance = interactable->GetEntranceRectangle();
-			var shape = GetShape();
-			entrance = Rectangle(interactable->GetX() + entrance[0], interactable->GetY() + entrance[1], entrance[2], entrance[3]);
-			shape = Rectangle(GetX() + shape[0], GetY() + shape[1], shape[2], shape[3]);
-			var overlap = Shape->Intersect(entrance, shape);
-			// Interactable can be entered if the area of overlap is bigger than zero.
-			can_be_entered = overlap->GetArea() > 0;
-			// Interactable can be entered if you are pushing a vehcile and the object is a container, see issue #1969
-			if (GetProcedure() == "PUSH")
+			for (var index = 0; index < interaction_count; index++)
 			{
-				can_be_entered &= (interactable->~IsContainer() || interactable->~AllowsVehicleEntrance());
+				PushBackInteraction(possible_interactions,
+					{
+						interaction_object = interactable,
+						priority = 9,
+						interaction_index = index,
+						extra_data = nil,
+						actiontype = ACTIONTYPE_SCRIPT
+					});
 			}
-		}
-		if (has_entrance && ((can_be_entered && !can_only_use_container) || can_be_exited))
-		{
-			var priority = 29;
-			if (can_be_exited)
-				priority = 0;
-			PushBackInteraction(possible_interactions,
-				{
-					interaction_object = interactable,
-					priority = priority,
-					interaction_index = nil,
-					extra_data = nil,
-					actiontype = ACTIONTYPE_STRUCTURE
-				});
 		}
 	}
 
@@ -474,68 +426,27 @@ func GetInteractableObjects(array sort)
 }
 
 // Returns the number of interactable objects, which is different from the total number of available interactions.
-private func GetInteractableObjectsCount()
+func GetInteractableObjectsCount()
 {
 	var interactions = GetInteractableObjects();
 	var interaction_objects = [];
 	for (var interaction in interactions)
+	{
 		PushBack(interaction_objects, interaction.interaction_object);
+	}
 	RemoveDuplicates(interaction_objects);
 	return GetLength(interaction_objects);
 }
 
-// executes interaction with an object. /action_info/ is a proplist as returned by GetInteractableObjects
+// Executes interaction with an object. /action_info/ is a proplist as returned by GetInteractableObjects
 func ExecuteInteraction(proplist action_info)
 {
 	if (!action_info.interaction_object)
+	{
 		return;
-
-	// object is a pushable vehicle
-	if (action_info.actiontype == ACTIONTYPE_VEHICLE)
-	{
-		var proc = GetProcedure();
-		// object is inside building -> activate
-		if (Contained() && action_info.interaction_object->Contained() == Contained())
-		{
-			SetCommand("Activate", action_info.interaction_object);
-			return true;
-		}
-		// crew is currently pushing vehicle
-		else if (proc == "PUSH")
-		{
-			// which is mine -> let go
-			if (GetActionTarget() == action_info.interaction_object)
-				ObjectCommand("UnGrab");
-			else
-				ObjectCommand("Grab", action_info.interaction_object);
-
-			return true;
-		}
-		// grab
-		else if (proc == "WALK")
-		{
-			ObjectCommand("Grab", action_info.interaction_object);
-			return true;
-		}
 	}
-	// object is a building
-	else if (action_info.actiontype == ACTIONTYPE_STRUCTURE)
-	{
-		// inside? -> exit
-		if (Contained() == action_info.interaction_object)
-		{
-			ObjectCommand("Exit");
-			return true;
-		}
-		// outside? -> enter
-		else if (this->CanEnter())
-		{
-			// First attempt to enter the pushed vehicle.
-			ObjectCommand("Enter", action_info.interaction_object, nil, nil, nil, C4CMD_Enter_PushTarget);
-			return true;
-		}
-	}
-	else if (action_info.actiontype == ACTIONTYPE_SCRIPT)
+
+	if (action_info.actiontype == ACTIONTYPE_SCRIPT)
 	{
 		if (action_info.interaction_object->~IsInteractable(this))
 		{
@@ -546,6 +457,8 @@ func ExecuteInteraction(proplist action_info)
 	else if (action_info.actiontype == ACTIONTYPE_EXTRA)
 	{
 		if (action_info.extra_data)
+		{
 			action_info.extra_data.Object->Call(action_info.extra_data.Fn, this);
+		}
 	}
 }

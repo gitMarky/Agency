861297898d0b7c915e944fbb088da7923c6f463f
Mark <markh4b@hotmail.com>
2019-07-07 19:17:42 +0200

    Correctly handle constraint
    Adjust by predicted position, instead of current position, so that
    AddVelocity makes sense. Otherwise you need SetVelocity, but this stops
    objects in mid-air, which looks strange.

--- Agency.ocd/System.ocg/Script_SpringConstraint.c
+++ Agency.ocd/System.ocg/Script_SpringConstraint.c
@@ -5,14 +5,17 @@
 */
 static const FxSpringConstraint = new Effect
 {
-	Start = func (int temporary, int distance, int max_dist)
+	Start = func (int temporary, int distance, int max_dist, int max_velocity)
 	{
 		if (temporary)
 		{
 			return;
 		}
-		this.distance = distance;
-		this.max_dist = max_dist;
+		this.prec_distance = 10;
+		this.prec_angle = 1000;
+		this.distance = distance * this.prec_distance;
+		this.max_dist = max_dist * this.prec_distance;
+		this.max_velocity = max_velocity ?? this.max_dist / 5;
 	},
 
 	SetBodyA = func (object body_a, bool fixed)
@@ -43,36 +46,43 @@ static const FxSpringConstraint = new Effect
 		{
 			return FX_Execute_Kill;
 		}
-		// Range exceeded?
-		var distance = ObjectDistance(this.body_a, this.body_b);
-		if (this.max_dist && distance >= this.max_dist)
+
+		// Range exceeded? => Stop!
+		var range = ObjectDistance(this.body_a, this.body_b) * this.prec_distance;
+		if (this.max_dist && range >= this.max_dist)
 		{
 			return FX_Execute_Kill;
 		}
+
+		// Calculate displacement, by predicting the next position
+		var x_body_a = this.body_a->GetX(this.prec_distance) + this.body_a->GetXDir(this.prec_distance);
+		var y_body_a = this.body_a->GetY(this.prec_distance) + this.body_a->GetYDir(this.prec_distance);
+		var x_body_b = this.body_b->GetX(this.prec_distance) + this.body_b->GetXDir(this.prec_distance);
+		var y_body_b = this.body_b->GetY(this.prec_distance) + this.body_b->GetYDir(this.prec_distance);
+		var distance = Distance(x_body_a, y_body_a, x_body_b, y_body_b);
+		var angle = Angle(x_body_a, y_body_a, x_body_b, y_body_b, this.prec_angle);
+
 		var displacement = distance - this.distance;
+
 		// Both ends loose? Assume a fixed point in the middle of the "spring"
 		if (!this.fixed_a && !this.fixed_b)
 		{
 			displacement = (displacement + 1) / 2;
 		}
 
-		Constrain(displacement);
+		Constrain(angle, displacement);
 	},
 
-	Constrain = func (int displacement)
+	Constrain = func (int angle, int velocity)
 	{
-		var prec_angle = 1000;
-		var angle = Angle(this.body_a->GetX(), this.body_a->GetY(), this.body_b->GetX(), this.body_b->GetY(), prec_angle);
-
-		var velocity = 10 * displacement;
-
+		velocity = BoundBy(velocity, -this.max_velocity, this.max_velocity);
 		if (!this.fixed_a)
 		{
-			this.body_a->AddVelocity(angle, velocity, prec_angle);
+			this.body_a->AddVelocity(angle, velocity, this.prec_angle, this.prec_distance);
 		}
 		if (!this.fixed_b)
 		{
-			this.body_b->AddVelocity((prec_angle * 180) + angle, velocity, prec_angle);
+			this.body_b->AddVelocity((this.prec_angle * 180) + angle, velocity, this.prec_angle, this.prec_distance);
 		}
 	},
 };

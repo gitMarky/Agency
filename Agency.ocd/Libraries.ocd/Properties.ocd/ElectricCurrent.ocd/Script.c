8b98dd084c47441ef82af4b1e28c7e6669142263
Mark <markh4b@hotmail.com>
2019-07-18 21:33:21 +0200

    Added more electricity logic

--- Agency.ocd/Libraries.ocd/Properties.ocd/ElectricCurrent.ocd/Script.c
+++ Agency.ocd/Libraries.ocd/Properties.ocd/ElectricCurrent.ocd/Script.c
@@ -2,15 +2,23 @@
 	Marks an object being able to electrify.
  */
 
-public func HasElectricCurrent(){ return true; }
+public func HasElectricCurrent(){ return this.ElectricCurrent; }
+public func CanBeElectrified(){  return !this.ElectricCurrent; }
 
 
-public func Electrify()
+local ElectricCurrent = true; // You can overwrite this, to switch between the modes. False = object can be electrified passively
+
+
+public func Electrify(object hazard)
 {
 	var current = GetEffect(FxElectricCurrent.Name, this);
 	if (current == nil)
 	{
 		current = CreateEffect(FxElectricCurrent, 100, 10); // Timer of 10 is OK, should be enough to electrocute
+		if (hazard)
+		{
+			current.Hazard = hazard;
+		}
 	}
 }
 
@@ -25,9 +33,21 @@ local FxElectricCurrent = new Effect
 {
 	Name = "ElectricCurrent",
 
-	// TODO: Timer has to look for objects that can be electrified, e.g. a puddle, and the puddle object will kill anyone with its own logic!
 	Timer = func ()
 	{
+		// Look for objects that can be electrified, e.g. a puddle
+		if (this.Target->~HasElectricCurrent() && !this.Hazard)
+		{
+			this.Hazard = this.Target->FindObject(Find_Or(Find_AtPoint(), Find_Distance(5)), Find_NoContainer(), Find_Func("CanBeElectrified"));
+		}
+
+		// Create effects at the target, and if we power a hazard
+		// then we forward effect calls and triggering calls to it
 		this.Target->~EffectsOnElectricCurrent();
+		if (this.Hazard)
+		{
+			this.Hazard->~EffectsOnElectricCurrent(); // Create effect
+			this.Hazard->~TriggerOnElectricCurrent(); // Logic for electrocuting victims
+		}
 	},
 };

5d190e16e3937eb59370e4b94131c98bfd4163d4
Mark <markh4b@hotmail.com>
2019-07-21 19:45:45 +0200

    Display only one item

--- Agency.ocd/Interface.ocd/HeadUpDisplay.ocd/Script.c
+++ Agency.ocd/Interface.ocd/HeadUpDisplay.ocd/Script.c
@@ -1,6 +1,6 @@
 // Include the different subsystems of the HUD. They all handle their part
 // themselves via overloading of callbacks.
-#include GUI_Controller_InventoryBar
+#include GUI_Agency_HUD_Inventory
 #include GUI_Agency_HUD_Interactions
 // Include the basic functionality of the HUD
 #include Library_HUDController
